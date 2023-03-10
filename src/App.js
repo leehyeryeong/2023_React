2e3de8d74cd0d0a07b8065205fd1633aea2ae4ab
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-10 12:11:05 +0900

    주석

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-
   return (
-    <div className="react">{name}</div>
+    <>
+    { /* 주석은 이렇게 작성합니다 */}
+    <div
+      className="react" // 시작태그를 여러 줄로 작성하게 된다면 주석을 작성
+    >{name}
+    </div>
+    // 하지만 이런 주석이나
+    /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
+    <input />
+    </>
   );
 }
 
