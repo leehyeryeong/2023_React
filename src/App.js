ccd71a24fa9b693931a397837f38c0b15aef103f
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-14 14:37:42 +0900

    클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  const name = '리액트';
-  return (
-    <>
-    { /* 주석은 이렇게 작성합니다 */}
-    <div
-      className="react" // 시작태그를 여러 줄로 작성하게 된다면 주석을 작성
-    >{name}
-    </div>
-    // 하지만 이런 주석이나
-    /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
-    <input />
-    </>
-  );
+class App extends Component {
+  render() {
+    const name = "리액트";
+    return <div className="react">{name}</div>;
+  }
 }
+// function App() {
+//   const name = "리액트";
+//   return <div className="react">{name}</div>;
+// }
 
 export default App;
