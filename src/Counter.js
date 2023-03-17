1a6786c236e6f9e228e318fd0d3c39f3d64c5dd1
leehyeryeong <w2108@e-mirim.hs.kr>
2023-03-17 12:01:54 +0900

    state 객체 안에 여러 값이 있을 경우

--- src/Counter.js
+++ src/Counter.js
@@ -5,27 +5,28 @@ class Counter extends Component {
     super(props);
     //state의 초깃값을 설정
     this.state = {
-      number : 0
+      number: 0,
+      fixedNumber: 0
     };
   }
-
   render() {
-    const { number } = this.state; //state를 조회할 때 this.state로 한다.
+    const { number, fixedNumber } = this.state; //state를 조회할 때 this.state로 한다.
     return (
       <div>
-        <h1>{ number }</h1>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정
-          onClick= {() => {
+          onClick={() => {
             //this.state를 사용하여 state에 새로운 값을 넣을 수 있다.
-            this.setState({ number: number + 1});
+            this.setState({ number: number + 1 });
           }}
-          >
+          > 
             +1
         </button>
       </div>
-    );
-  }
+    ); 
+  } 
 }
 
 export default Counter;
