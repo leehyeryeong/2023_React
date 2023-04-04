d24d02cff6c0c546b029cbbe7a094aeb9c1815e2
leehyeryeong <w2108@e-mirim.hs.kr>
2023-04-04 16:13:15 +0900

    초기 상태 설정하기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,7 +1,19 @@
-const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name) => <li>{name}</li>);
+import { useState } from "react";
 
+const IterationSample = () => {
+  const [names, setNames] = //eslint-disable-line no-unused-vars
+    useState([
+      { id: 1, text: "눈사람" },
+      { id: 2, text: "얼음" },
+      { id: 3, text: "눈" },
+      { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState(""); //eslint-disable-line no-unused-vars
+  const [nextId, setNextId] = useState(5); //eslint-disable-line no-unused-vars
+
+  //새로운 항목을 추가할 때 사용할 id
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  
   return <ul>{nameList}</ul>;
 };
 
