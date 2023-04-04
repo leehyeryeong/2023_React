b26733300ea0473b5175d6b026ebb681e0f9a7d7
leehyeryeong <w2108@e-mirim.hs.kr>
2023-04-04 15:39:13 +0900

    컴포넌트 반복

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Component } from "react";
-import ScrollBox from "./ScrollBox";
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)}/>
-        <button onClick={() => this.ScrollBox.scrollBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    )
-  }
-}
+import IterationSample from "./IterationSample";
+
+const App = () => {
+  return <IterationSample />;
+};
 
 export default App;
